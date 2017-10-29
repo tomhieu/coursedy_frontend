5df796e45469fb920da3f60e5dfe6d38e1f87476
Trung Pham <pdbaotrung@gmail.com>
2017-10-29 12:55:02 +0700

    Implement tutor account page

--- client/src/validations/SignUpFormValidation.js
+++ client/src/validations/SignUpFormValidation.js
@@ -11,7 +11,6 @@ export const asyncValidate = (values) => {
 
 export const validate = (values) => {
   const errors = {}
-  debugger
   if (!values.role) {
     errors.role = TT.t('role_required')
   }
