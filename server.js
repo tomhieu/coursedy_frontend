7aa623f15a047a1dd1a743668b2cb3160e1349ff
Hieu Phan <ptrhieu.it@gmail.com>
2017-11-04 22:48:04 +0700

    always serve static file (index.html) on production

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
 app.use(express.static('client/dist'));
+app.get('/*', function (req, res) {
+  res.sendFile(`${__dirname}/client/dist/index.html`)
+})
 app.listen(port)
