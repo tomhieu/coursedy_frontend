ccd8f6e4c08db586e09fe574787af64a2ab16b6f
tinhuynh <tinhuynh0992@gmail.com>
2018-04-05 10:03:29 +0700

    Change course status constant

--- client/src/constants/WebConstants.js
+++ client/src/constants/WebConstants.js
@@ -4,9 +4,9 @@ export const HIDE_FOOTER = "HIDE_FOOTER";
 
 
 //Student
-export const UNSTARTED_COURSE = 0;
-export const STARTED_COURSE = 1;
-export const TERMINATED_COURSE = 2;
+export const UNSTARTED_COURSE = "not_started";
+export const STARTED_COURSE = "started";
+export const TERMINATED_COURSE = "finished";
 
 
 
