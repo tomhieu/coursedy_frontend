8fe1cac8ba2c00f9519debd6f362c9bb6edaf1a6
TanNguyen <mntan998@gmail.com>
2018-04-08 16:50:05 +0700

    Add pagination teachers page

--- client/src/containers/ListTeacher/PublicTeacherListContainer.js
+++ client/src/containers/ListTeacher/PublicTeacherListContainer.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import * as TeacherActions from '../../actions/TeacherCreators';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import UserAvatar from '../../components/Account/UserAvatar';
 import TeacherItem from './TeacherItem';
 import { searchTeachers } from '../../actions/TeacherCreators';
+import Pagination from 'react-js-pagination';
 
 
 class PublicTeacherListContainer extends Component {
@@ -13,20 +10,47 @@ class PublicTeacherListContainer extends Component {
     this.props.searchTeachers();
   }
 
+  handlePageChange(pageNumber) {
+    this.props.searchTeachers({page: pageNumber, per_page: this.props.headers.perPage});
+  }
+
   render() {
     return (
-        <div className="row">
-          {
-            this.props.teachers.data.map((item) => {
-              if (!item.user) {
-                return null;
-              }
-              return <TeacherItem data={item} key={item.id}/>
-            })
-          }
-        </div>
+      <div>
+        <TeacherList data={this.props.teachers.data}/>
+        {
+          !this.props.teachers.loading && this.props.teachers.data.length ?
+            <div className="row">
+              <div className="col-sm-12">
+                <Pagination
+                  activePage={this.props.headers.currentPage}
+                  itemsCountPerPage={this.props.headers.perPage}
+                  totalItemsCount={this.props.headers.total}
+                  pageRangeDisplayed={5}
+                  onChange={this.handlePageChange.bind(this)}
+                />
+              </div>
+            </div>
+            :null
+        }
+      </div>
     );
   };
+}
+
+const TeacherList = ({data}) => {
+  return (
+    <div className="row">
+      {
+       data.map((item) => {
+          if (!item.user) {
+            return null;
+          }
+          return <TeacherItem data={item} key={item.id}/>
+        })
+      }
+    </div>
+  )
 }
 
 
@@ -35,7 +59,16 @@ PublicTeacherListContainer.contextTypes = {
 };
 
 const mapStateToProps = (state) => {
-  return { teachers: state.Teachers };
+  const headers = state.Teachers.headers
+
+  return {
+    teachers: state.Teachers,
+    headers: {
+      currentPage: headers && parseInt(headers.xPage) || 0,
+      perPage: headers && parseInt(headers.xPerPage) || 0,
+      total: headers && parseInt(headers.xTotal) || 0
+    }
+  };
 };
 
 export default connect(mapStateToProps, { searchTeachers })(PublicTeacherListContainer);
--- client/src/reducers/TeachersReducer.js
+++ client/src/reducers/TeachersReducer.js
@@ -12,17 +12,20 @@ const Teachers = (state = {
   suggestions: [],
   filters: {selectedCategories: [], selectedSpecializes: [], term: ''},
   showSuggestion: false,
-  loadingSuggestion: false
+  loadingSuggestion: false,
+  headers: null
 }, action) => {
   switch (action.type) {
     case types.FETCH_TEACHERS + asyncActs.PENDING:
       return {...state, loading: true, error: null}
     case types.FETCH_TEACHERS + asyncActs.FULFILLED:
       return {...state, data: action.payload, loading: false, error: null}
+    case types.FETCH_TEACHERS + asyncActs.HEADERS:
+      return {...state, headers: action.payload, loading: true, error: null}
     case types.FETCH_TEACHERS + asyncActs.REJECTED:
       return {...state, loading: false, error: action.error, data:[], total: 0}
     /**
-     * handle action to load suggestion to filter Course
+     * Handle action to load suggestion to filter Course
      */
     case asyncActs.LOAD_SUGGESTION_TEACHERS + asyncActs.FULFILLED:
       return {...state, suggestions: action.payload, showSuggestion: true, loadingSuggestion: false}
