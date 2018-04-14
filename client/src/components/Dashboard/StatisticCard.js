ed08c230aa8b488c54bde7a46beaf8f914d624c7
tinhuynh <tinhuynh0992@gmail.com>
2018-04-14 11:08:43 +0700

    Add protypes for StatisticCard component

--- client/src/components/Dashboard/StatisticCard.js
+++ client/src/components/Dashboard/StatisticCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class StatisticCard extends Component {
   render() {
     const { value, title, isRedirect, redirectUrl, redirectText, subClasses } = this.props
+    //FIXME: subClasses is special situation for this theme. Need to replace later
     return (
       <div className="col-md-4 pad10">
         <div className={'dash-block ' + subClasses}>
@@ -20,6 +21,12 @@ class StatisticCard extends Component {
       </div>
     )
   }
+}
+
+StatisticCard.propTypes = {
+  value: React.PropTypes.number.isRequired,
+  title: React.PropTypes.string.isRequired,
+  isRequired: React.PropTypes.bool.isRequired,
 }
 
 export default StatisticCard
