150b92d64204c6ec80852cab6cabdfa76fecd131
Hieu Phan <ptrhieu.it@gmail.com>
2018-04-15 18:53:17 +0700

    comment out role authorization

--- client/src/components/RoleAuthorization.js
+++ client/src/components/RoleAuthorization.js
@@ -5,8 +5,8 @@ import {globalHistory} from "../utils/globalHistory";
 class RoleAuthorization extends Component {
   componentWillMount(){
     if (this.props.currentUser != null) {
-        const authorized = this.authorizedRoles.map((role) => this.props.currentUser.roles.indexOf(role) >= 0).reduce((x, y) => x || y);
-        if (!authorized) globalHistory.replace(this.unauthorizedPath);
+        // const authorized = this.authorizedRoles.map((role) => this.props.currentUser.roles.indexOf(role) >= 0).reduce((x, y) => x || y);
+        // if (!authorized) globalHistory.replace(this.unauthorizedPath);
     } else {
         this.props.dispatch(Actions.checkRole(this.authorizedRoles, this.unauthorizedPath));
     }
