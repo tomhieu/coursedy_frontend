35bc350c6ec262c0659419359fd62775f94e281c
Hieu Phan <ptrhieu.it@gmail.com>
2018-04-28 20:33:03 +0700

    update course form style

--- client/src/components/Courses/CourseForm/CourseForm.js
+++ client/src/components/Courses/CourseForm/CourseForm.js
@@ -19,7 +19,7 @@ class CourseForm extends Component {
     return (
       <div>
         <form onSubmit={handleSubmit(this.props.onSubmit)} multiple={true}>
-          <div className={editMode ? "d-flex flex-horizontal flex-auto justify-content-center mt-30 mb-30" : "d-flex flex-horizontal"}>
+          <div className={editMode ? "d-flex flex-horizontal flex-auto justify-content-center mb-30" : "d-flex flex-horizontal"}>
             {
               editMode ? (
                 <div className={styles.avatarImage}>
