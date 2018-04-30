734a21e8eba2eb3e8e913e7f1e870f1abb50d9cd
Hieu Phan <ptrhieu.it@gmail.com>
2018-04-30 17:02:19 +0700

    fix register, login form

--- client/src/pages/LoginRegisterPage/LoginRegisterPage.js
+++ client/src/pages/LoginRegisterPage/LoginRegisterPage.js
@@ -5,19 +5,17 @@ import {LoginFormContainer, RegisterFormContainer} from '../../containers/index'
 import LoadingMask from "../../components/LoadingMask/LoadingMask";
 
 const LoginRegisterForm = (props) => (
-  <div className='login-register d-flex flex-auto'>
-    <div className='container'>
-      <div className='row row-padding'>
-        <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-          <LoadingMask placeholderId="loginPlaceholder" isFullLoading={true}>
-            <LoginFormContainer {...props} />
-          </LoadingMask>
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-          <LoadingMask placeholderId="registerPlaceholder">
-            <RegisterFormContainer {...props} />
-          </LoadingMask>
-        </div>
+  <div className='login-register'>
+    <div className='row mb-20'>
+      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <LoadingMask placeholderId="loginPlaceholder" isFullLoading={true}>
+          <LoginFormContainer {...props} />
+        </LoadingMask>
+      </div>
+      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <LoadingMask placeholderId="registerPlaceholder">
+          <RegisterFormContainer {...props} />
+        </LoadingMask>
       </div>
     </div>
   </div>
