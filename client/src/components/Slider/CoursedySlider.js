1b010008d3460cb1b802e4e6dbb4d6d3577e2a39
Trung Pham <pdbaotrung@gmail.com>
2018-06-23 09:43:37 +0700

    update UI

--- client/src/components/Slider/CoursedySlider.js
+++ client/src/components/Slider/CoursedySlider.js
@@ -10,16 +10,41 @@ class CoursedySlider extends Component {
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 300,
       slidesToShow: 4,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     const {items} = this.props;
     return (
       <Slider {...settings}>
         {
           items.map((item, index) => {
-            return <div className={styles.itemContainer} key={index}>{item}</div>
+            return <div key={index}>{item}</div>
           })
         }
       </Slider>
