6f0054b5e6af3192927fd0d1133b98a138fd939d
TanNguyen <mntan998@gmail.com>
2018-08-19 17:24:12 +0700

    View more course detail introduction

--- client/src/components/Core/EmptyResultWarning.js
+++ client/src/components/Core/EmptyResultWarning.js
@@ -1,43 +1,50 @@
-import React from 'react'
-import {TT} from "utils/locale";
-import {Component} from "react";
+import React from 'react';
+import { TT } from 'utils/locale';
+import { Component } from 'react';
+
 
 class EmptyResultWarning extends Component {
   resetFilter() {
     this.props.resetFilter();
-    this.props.search({currentPage: 1, sortOrder: 'desc'});
+    this.props.search({ currentPage: 1, sortOrder: 'desc' });
   }
+
   render() {
-    const {styles, searchType} = this.props;
+    const { styles, searchType } = this.props;
     return (
       <div className={`${styles} row justify-content-center`}>
         <div className="d-flex flex-auto align-items-center">
           <div className="d-flex flex-vertical flex-auto">
-            <div className="empty-result-message">{this.context.t("search_empty_result", {type: <strong>{this.context.t(searchType)}</strong>})}</div>
+            <div
+              className="empty-result-message">{this.context.t('search_empty_result', {
+              type: <strong>{this.context.t(searchType)}</strong>
+            })}</div>
             <div className="list-option">
               <ul className="empty-result-option">
-                <li>{TT.t("search_empty_result_header")}</li>
-                <li><a className="clear-filter" tabIndex={1} onClick={this.resetFilter.bind(this)}>{TT.t("search_empty_result_reset")}</a></li>
+                <li>{TT.t('search_empty_result_header')}</li>
+                <li><a className="clear-filter" tabIndex={1}
+                       onClick={this.resetFilter.bind(this)}>{TT.t('search_empty_result_reset')}</a>
+                </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
+
 EmptyResultWarning.contextTypes = {
   t: React.PropTypes.func.isRequired
-}
+};
 
 EmptyResultWarning.defaultProps = {
   styles: ''
-}
+};
 
 EmptyResultWarning.propTypes = {
-  styles: React.PropTypes.string,
-  searchType: React.PropTypes.string,
-}
+  styles: React.PropTypes.string, searchType: React.PropTypes.string
+};
 
-export default EmptyResultWarning
+export default EmptyResultWarning;
