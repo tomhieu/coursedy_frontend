8c697aedc093001cb1a2915d884337bade241ce4
tinhuynh <tinhuynh0992@gmail.com>
2018-09-22 15:23:54 +0700

    Use loading icon

--- client/src/containers/LoadingMask/AsyncLoader.js
+++ client/src/containers/LoadingMask/AsyncLoader.js
@@ -21,9 +21,7 @@ class AsyncLoader extends Component {
       return (
         <div className="section-placeholder">
           <div id="spinner">
-            <svg className="circular" viewBox="25 25 50 50">
-              <circle className="path" cx="50" cy="50" r="20" fill="none" />
-            </svg>
+            <LoadingIcon />
           </div>
         </div>
       );
@@ -31,9 +29,7 @@ class AsyncLoader extends Component {
       return (
         <div className="button-placeholder">
           <div id="spinner">
-            <svg className="circular" viewBox="25 25 50 50">
-              <circle className="path" cx="50" cy="50" r="20" fill="none" />
-            </svg>
+            <LoadingIcon />
           </div>
         </div>
       );
