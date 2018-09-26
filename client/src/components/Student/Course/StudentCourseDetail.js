0f75e678c0fe8028871ad6df8655afd29237aba8
Trung Pham <pdbaotrung@gmail.com>
2018-09-26 21:33:32 +0700

    implement student course details

---
+++ client/src/components/Student/Course/StudentCourseDetail.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import CourseDetailHeader from './CourseDetail/CourseDetailHeader';
+import CourseDetailMain from './CourseDetail/CourseDetailMain';
+import './CourseDetail.scss';
+import {PUBLIC_COURSE_MAX_NUMBER_COMMENTS_PER_LOAD} from '../../constants/Courses';
+import CourseDetailAction from './CourseDetail/CourseDetailAction';
+import {globalHistory} from '../../utils/globalHistory';
+
+/**
+  * @Course group template 2
+  * @Use for CoursePage
+  */
+class StudentCourseDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayFixedSidebar: false,
+      currentScrollPosition: 0,
+      activeMenu: 'course-detail-intro',
+      showEnrollPopup: false,
+      showEnrollSuccessPopup: false
+    };
+    this.onScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll);
+    window.scrollTo(0, 0);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  handleScroll(event) {
+    const triggerPosition = 200;
+    const top = window.pageYOffset || document.documentElement.scrollTop;
+    if (triggerPosition < top) {
+      this.courseActionBar.classList.add('fixed-action-bar');
+    } else {
+      this.courseActionBar.classList.remove('fixed-action-bar');
+    }
+  }
+
+  enrollToCourse(courseId, user) {
+    const res = this.props.enrollCourse(courseId);
+    res.then(() => {
+      this.props.fetchEnrolledCourseList(user);
+      this.closeEnrollPopup();
+      this.setState({ showEnrollSuccessPopup: true });
+    }, (error) => {
+      globalHistory.push('payment');
+    });
+  }
+
+  openEnrollPopup() {
+    this.setState({ showEnrollPopup: true });
+  }
+
+  closeEnrollPopup() {
+    this.setState({ showEnrollPopup: false });
+  }
+
+  closeEnrollSuccessPopup() {
+    this.setState({ showEnrollSuccessPopup: false });
+  }
+
+  render() {
+    const { activeMenu, currentScrollPosition } = this.state;
+    const { course, course_sections, user } = this.props;
+    return (
+      <div className="d-flex flex-auto flex-vertical public-course-details">
+        <CourseDetailHeader
+          {...this.props}
+        />
+        <div className="d-flex flex-auto course-details-container">
+          <div className="d-flex flex-auto course-details-wrapper container">
+            <div className="course-action-mobile-view">
+              <CourseDetailAction
+                course={course}
+                course_sections={course_sections}
+                openEnrollCoursePopup={this.openEnrollPopup.bind(this)}
+                {...this.props}
+              />
+            </div>
+            <div className="d-flex flex-auto course-content">
+              <CourseDetailMain
+                {...this.props}
+                activeMenu={activeMenu}
+                currentScrollPosition={currentScrollPosition}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+StudentCourseDetail.contextTypes = {
+  t: React.PropTypes.func.isRequired
+};
+
+StudentCourseDetail.propTypes = {
+  enrollCourse: React.PropTypes.func.isRequired,
+  fetchEnrolledCourseList: React.PropTypes.func.isRequired,
+  isEnrolled: React.PropTypes.bool.isRequired
+};
+
+export default StudentCourseDetail;
---
+++ client/src/containers/Student/Dashboard/Courses/StudentCourseDetailContainer.js
@@ -0,0 +1,154 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as WebConstants from 'constants/WebConstants';
+import {CourseDetail} from '../../../components/index';
+import * as PublicCourseActions from '../../../actions/PublicCourseActionCreator';
+import * as ReferActions from '../../../actions/ReferenceActions/ReferenceDataActionCreator';
+import {openConfirmationPopup} from '../../../actions/MainActionCreator';
+import PageContainer from '../../../utils/PageContainer';
+import * as sessionActions from '../../../actions/SessionActionCreator';
+
+class StudentCourseDetailContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.getCourseCategories();
+    const { courseId, course_comments_page } = this.props;
+    if (courseId) {
+      // Fetch course
+      this.props.getPublicCourse(courseId);
+
+      // Fetch comments
+      this.props.getCourseComments(courseId, course_comments_page);
+
+      // Fetch related courses
+      this.props.getRelatedCourses(courseId, WebConstants.START_PAGE_INDEX, WebConstants.RELATED_COURSE_PER_PAGE);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.lang !== nextProps.lang) {
+      this.props.getCourseCategories();
+      const { courseId } = nextProps;
+      if (courseId) {
+        // Fetch course
+        this.props.getPublicCourse(courseId);
+
+        // Fetch related courses
+        this.props.getRelatedCourses(courseId, WebConstants.START_PAGE_INDEX, WebConstants.RELATED_COURSE_PER_PAGE);
+      }
+    }
+  }
+
+  componentWillUnmount() {
+  }
+
+  loadMoreComments() {
+    this.props.getCourseComments(this.props.courseId, this.props.course_comments_page + 1);
+  }
+
+  changeActiveMenu(payload) {
+    this.props.changeActiveMenu(payload);
+  }
+
+  render() {
+    return (
+      <PageContainer
+        error={this.props.course.error}
+        meta={{ title: this.context.t('course_detail_page', { title: this.props.course.title || '' }) }}
+      >
+        <CourseDetail
+          {...this.props}
+          loadMoreCommentsHdl={this.loadMoreComments.bind(this)}
+          changeActiveMenu={this.changeActiveMenu.bind(this)}
+        />
+      </PageContainer>
+    );
+  }
+}
+
+PublicCourseDetailContainer.contextTypes = {
+  t: React.PropTypes.func.isRequired
+};
+
+PublicCourseDetailContainer.propTypes = {
+
+};
+
+const getCourseCategory = (categories, course) => {
+  if (course && categories && categories.length > 0) {
+    return categories.filter((category) => {
+      return category.id == course.category_id;
+    })[0];
+  }
+  return null;
+};
+
+const getCourseLevel = (categories, course) => {
+  const category = getCourseCategory(categories, course);
+  if (category) {
+    return category.course_levels.filter((level) => {
+      return level.id == course.course_level_id;
+    })[0];
+  }
+  return null;
+};
+
+const getCourse = (course, courseSections) => {
+  course = { ...course };
+  let totalPeriod = 0;
+  courseSections.forEach((section) => {
+    section.lessons.forEach((lesson) => {
+      totalPeriod += lesson.period;
+    });
+  });
+  course.totalPeriod = totalPeriod;
+  return course;
+};
+
+const mapStateToProps = (state) => {
+  const categories = state.referenceData.courseCategories;
+  const {
+    course,
+    relatedCourses,
+    course_tutor,
+    course_sections,
+    course_comments,
+    course_comments_page,
+    sectionPositions
+  } = state.PublicCourseDetail;
+  const { newStartedCourses } = state.session;
+  const isEnrolled = newStartedCourses.findIndex(c => c.id === course.id) >= 0;
+  return {
+    course_category: getCourseCategory(categories, course),
+    course_level: getCourseLevel(categories, course),
+    user: state.session.currentUser,
+    categories,
+    course: getCourse(course, course_sections),
+    relatedCourses,
+    course_tutor,
+    course_sections,
+    course_comments,
+    course_comments_page,
+    sectionPositions,
+    isEnrolled
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  getCourseCategories: () => dispatch(ReferActions.fetchCourseCategories()),
+  getPublicCourse: courseId => dispatch(PublicCourseActions.fetchPublicCourse(courseId)),
+  getCourseComments: (courseId, page) => dispatch(PublicCourseActions.fetchCourseComments(courseId, page)),
+  getRelatedCourses: (courseId, page, perPage) => dispatch(PublicCourseActions.fetchRelatedCourses({course_id: courseId, page, perPage})),
+  enrollCourse: (courseId) => dispatch(PublicCourseActions.submitEnrollCourse(courseId)),
+  addCourseToCart: (course) => dispatch(PublicCourseActions.addCourseToCart(course)),
+  changeActiveMenu: (payload) => dispatch(PublicCourseActions.changeActiveMenu(payload)),
+  showWarningPopup: (title, message, callback) => dispatch(openConfirmationPopup(title, message, callback)),
+  fetchEnrolledCourseList: user => dispatch(sessionActions.fetchActiveCourses(user))
+});
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(StudentCourseDetailContainer);
