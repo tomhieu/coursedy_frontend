ee3b15c6ba2f5f5c613e0a3ff5ac731c3c06b83c
Trung Pham <pdbaotrung@gmail.com>
2018-09-29 12:57:56 +0700

    fixed missing pre-validation password validator

--- client/src/validations/ChangePasswordFormValidator.js
+++ client/src/validations/ChangePasswordFormValidator.js
@@ -2,11 +2,13 @@ import { validateMandatoryField, validatePassword } from './CommonValidator';
 
 export const validate = (values) => {
   const errors = {};
-  validateMandatoryField('current_password', values.current_password, 'current_password_required', errors);
+  if (!values) {
+    return errors;
+  }
 
+  validateMandatoryField('current_password', values.current_password, 'current_password_required', errors);
   validateMandatoryField('password', values.password, 'password_required', errors);
   validatePassword(values.password, values.password_confirmation, 'password', errors);
-
   validateMandatoryField('password_confirmation', values.password_confirmation, 'password_confirmation_required', errors);
 
   return errors;
